a47dc6748b1b8ea98c32b690d4d74765ae699249
Bhagirath Makwana <83909096+Bhagi-developer@users.noreply.github.com>
2021-07-13 09:53:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ $(document).ready(() => {
         }, 10);
         
     })
+    
+     $(".notify").click(() => {
+        $(".notifyMenue").toggleClass("showNotify");
+
+       
+    })
 
     function declare(){
         alert("Hey use! You are using vitual windows11!!!");
